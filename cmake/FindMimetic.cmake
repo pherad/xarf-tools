5e665420623413be014f6cff3cf160f0b5b3ac54
Alexander Haase <alexander.haase@rwth-aachen.de>
2015-05-07 20:59:53 +0200

    First commit for libxarf

---
+++ cmake/FindMimetic.cmake
@@ -0,0 +1,43 @@
+# This file is part of X-ARF tools.
+#
+# X-ARF tools is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License and GNU
+# Lesser General Public License along with this program. If not, see
+#
+# http://www.gnu.org/licenses/
+#
+#
+# Copyright (C)
+#   2015 Alexander Haase <alexander.haase@rwth-aachen.de>
+#
+
+#
+# include required modules
+#
+include(FindPackageHandleStandardArgs)
+
+
+# search for libmimetic
+find_path(MIMETIC_INCLUDE_DIR mimetic/mimetic.h)
+find_library(MIMETIC_LIBRARY mimetic)
+
+# process found items
+find_package_handle_standard_args(MIMETIC DEFAULT_MSG
+	MIMETIC_INCLUDE_DIR
+	MIMETIC_LIBRARY
+)
+
+# set variables for user
+if (MIMETIC_FOUND)
+	set(MIMETIC_INCLUDE_DIRS ${MIMETIC_INCLUDE_DIR})
+	set(MIMETIC_LIBRARIES ${MIMETIC_LIBRARY})
+endif (MIMETIC_FOUND)
